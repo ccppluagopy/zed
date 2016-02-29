54f57590d7480b097b08f46a34a7e0d378f13580
cokeboL <165128167@qq.com>
2016-02-29 17:26:44 +0800

    -

---
+++ logger.go
@@ -0,0 +1,205 @@
+package zed
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type logtask struct {
+	running  bool
+	chMsg    chan *string
+	taskType string
+}
+
+var (
+	tags         map[int]string
+	debug        bool
+	maxTagNum    = 0
+	infoEnabled  = true
+	warnEnabled  = true
+	errorEnabled = true
+
+	infoCount  = 0
+	warnCount  = 0
+	errorCount = 0
+
+	infoLoggerNum int = 1
+	arrTaskInfo   []*logtask
+
+	warnLoggerNum int = 1
+	arrTaskWarn   []*logtask
+
+	errorLoggerNum int = 1
+	arrTaskError   []*logtask
+
+	logSep = ""
+
+	Printf  = fmt.Printf
+	Println = fmt.Println
+)
+
+func (task *logtask) start(taskType string) {
+	task.running = true
+	task.taskType = taskType
+	task.chMsg = make(chan *string, 100)
+
+	go func() {
+		var s *string
+		for {
+			select {
+			case s = <-task.chMsg:
+				if s == nil {
+					task.running = false
+					return
+				}
+				Println(*s)
+			}
+		}
+	}()
+	fmt.Printf("Logger Start Log %s Task\n", taskType)
+}
+
+func (task *logtask) stop() {
+	close(task.chMsg)
+}
+
+func Info(tag int, loggerIdx int, format string, v ...interface{}) {
+	if infoEnabled {
+		if debug {
+			if tagstr, ok := tags[tag]; ok {
+				s := strings.Join([]string{"[Info] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+				infoCount++
+				loggerIdx = loggerIdx % infoLoggerNum
+				arrTaskInfo[loggerIdx].chMsg <- &s
+			}
+		} else {
+			if tag < maxTagNum {
+				if tagstr, ok := tags[tag]; ok {
+					s := strings.Join([]string{"[Info] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+					infoCount++
+					loggerIdx = loggerIdx % infoLoggerNum
+					arrTaskInfo[loggerIdx].chMsg <- &s
+				}
+			}
+		}
+	}
+}
+
+func Warn(tag int, loggerIdx int, format string, v ...interface{}) {
+	//if warnEnabled && (tags[tag] == true) {
+	if warnEnabled {
+		if debug {
+			if tagstr, ok := tags[tag]; ok {
+				s := strings.Join([]string{"[Warn] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+				warnCount++
+				loggerIdx = loggerIdx % warnLoggerNum
+				arrTaskWarn[loggerIdx].chMsg <- &s
+			}
+		} else {
+			if tag < maxTagNum {
+				if tagstr, ok := tags[tag]; ok {
+					s := strings.Join([]string{"[Warn] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+					warnCount++
+					loggerIdx = loggerIdx % warnLoggerNum
+					arrTaskWarn[loggerIdx].chMsg <- &s
+				}
+			}
+		}
+	}
+}
+
+func Error(tag int, loggerIdx int, format string, v ...interface{}) {
+	if errorEnabled {
+		if debug {
+			if tagstr, ok := tags[tag]; ok {
+				s := strings.Join([]string{"[Error] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+				errorCount++
+				loggerIdx = loggerIdx % errorLoggerNum
+				arrTaskError[loggerIdx].chMsg <- &s
+			}
+		} else {
+			if tag < maxTagNum {
+				if tagstr, ok := tags[tag]; ok {
+					s := strings.Join([]string{"[Error] [", tagstr, "] ", fmt.Sprintf(format, v...)}, logSep)
+					errorCount++
+					loggerIdx = loggerIdx % errorLoggerNum
+					arrTaskError[loggerIdx].chMsg <- &s
+				}
+			}
+		}
+	}
+}
+
+func Start(isDebug bool, maxTag int, logtags map[int]string, infoLogNum int, warnLogNum int, errorLogNum int) {
+	debug = isDebug
+	maxTagNum = maxTag
+	tags = logtags
+	infoLoggerNum = infoLogNum
+	warnLoggerNum = warnLogNum
+	errorLoggerNum = errorLogNum
+
+	infoEnabled = (infoLogNum > 0)
+	warnEnabled = (warnLogNum > 0)
+	errorEnabled = (errorLogNum > 0)
+
+	arrTaskInfo = make([]*logtask, infoLoggerNum)
+
+	var i int
+	for i = 0; i < infoLoggerNum; i++ {
+		arrTaskInfo[i] = new(logtask)
+		arrTaskInfo[i].start("Info")
+	}
+
+	arrTaskWarn = make([]*logtask, warnLoggerNum)
+	for i = 0; i < warnLoggerNum; i++ {
+		arrTaskWarn[i] = new(logtask)
+		arrTaskWarn[i].start("Warn")
+	}
+
+	arrTaskError = make([]*logtask, errorLoggerNum)
+	for i = 0; i < errorLoggerNum; i++ {
+		arrTaskError[i] = new(logtask)
+		arrTaskError[i].start("Error")
+	}
+}
+
+func Stop() {
+	for {
+	REP:
+		for _, task := range arrTaskInfo {
+			task.stop()
+		}
+
+		for _, task := range arrTaskWarn {
+			task.stop()
+		}
+
+		for _, task := range arrTaskError {
+			task.stop()
+		}
+
+		time.Sleep(time.Second / 10)
+
+		for _, task := range arrTaskInfo {
+			if task.running {
+				goto REP
+			}
+		}
+
+		for _, task := range arrTaskWarn {
+			if task.running {
+				goto REP
+			}
+		}
+
+		for _, task := range arrTaskError {
+			if task.running {
+				goto REP
+			}
+		}
+
+		Println("[ShutDown] Logger Stop!")
+		return
+	}
+}
