a965e18fe63f8cd46e8547b11175e1f64380816b
cokeboL <165128167@qq.com>
2016-03-31 10:34:38 +0800

    1

--- test/test.go
+++ test/test.go
@@ -142,13 +142,13 @@ func TestEchoClientForTcpServer(addr string, clientNum int) {
 
 func TestBase() {
 	addr := "127.0.0.1:9999"
-	test.TestLogger()
-	test.TestEventMgr()
+	TestLogger()
+	TestEventMgr()
 
 	go func() {
 		time.Sleep(time.Second)
-		test.TestEchoClientForTcpServer(addr, 10)
+		TestEchoClientForTcpServer(addr, 10)
 	}()
 
-	test.TestTcpServer(addr)
+	TestTcpServer(addr)
 }
