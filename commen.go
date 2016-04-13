68992a205fc5f5c3be242f65cc8087294f77463a
cokeboL <165128167@qq.com>
2016-04-13 14:04:28 +0800

    1

--- commen.go
+++ commen.go
@@ -1,5 +1,9 @@
 package zed
 
+import (
+	"reflect"
+)
+
 type ClosureCB func()
 
 type TimerCallBack func()
@@ -12,9 +16,33 @@ type MsgHandler func(msg *NetMsg) bool
 
 type ClientCloseCB func(client *TcpClient)
 
+/*
 func NewCoroutine(cb ClosureCB) {
 	go func() {
 		defer PanicHandle(true)
 		cb()
 	}()
+}
+*/
+func NewCoroutine(cb interface{}, args ...interface{}) {
+	f := reflect.ValueOf(cb)
+	if f.Kind() == reflect.Func {
+		go func() {
+			defer PanicHandle(true)
+			n := len(args)
+			if n > 0 {
+				refargs := make([]reflect.Value, n)
+				for i := 0; i < n; i++ {
+					refargs[i] = reflect.ValueOf(args[i])
+				}
+				f.Call(refargs)
+
+			} else {
+				f.Call(nil)
+			}
+		}()
+	} else {
+		ZLog("NewCoroutine Error: cb is not function")
+		Println("NewCoroutine Error: cb is not function")
+	}
 }
