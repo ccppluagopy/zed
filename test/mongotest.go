4558ed166cbcc9e01cd399bf03379ecdfcb794c7
cokeboL <165128167@qq.com>
2016-08-31 17:03:41 +0800

    1

--- test/mongotest.go
+++ test/mongotest.go
@@ -21,25 +21,49 @@ type Student struct {
 
 func MongoInsert(idx int, pool *zed.MongoMgrPool, ch chan int) {
 	idx = idx * ACTION_NUM * 10
-	ok := true
 	for i := 0; i < ACTION_NUM; i++ {
+		time.Sleep(time.Second * 3)
 		pool.DBAction(idx/10/ACTION_NUM, func(collection *mgo.Collection) {
 			err := collection.Insert(&Student{fmt.Sprintf("stu_%d", idx+i), 20 + idx/10000})
+			fmt.Println("Insert err: ", err)
 			if err != nil {
-				fmt.Println("Insert err: ", err)
-				ok = false
+				panic(err)
 			}
 		})
-
-		if !ok {
-			break
-		}
 	}
 
 	ch <- 0
 }
 
 func main() {
+	zed.Init("./", "./log")
+	//zed.MakeNewLogDir("./")
+	const (
+		TagNull = iota
+		Tag1
+		Tag2
+		Tag3
+		TagMax
+	)
+	var LogTags = map[int]string{
+		TagNull: "zed",
+		Tag1:    "Tag1",
+		Tag2:    "Tag2",
+		Tag3:    "Tag3",
+	}
+	var LogConf = map[string]int{
+		//"Info":   zed.LogCmd,
+		"Info":   zed.LogFile,
+		"Warn":   zed.LogFile,
+		"Error":  zed.LogFile,
+		"Action": zed.LogFile,
+	}
+
+	zed.StartLogger(LogConf, true, TagMax, LogTags, 3, 3, 3, 3)
+	for i := 0; i < 5; i++ {
+		zed.LogError(Tag1, i, "log test %d", i)
+	}
+
 	ch := make(chan int)
 	pool := zed.NewMongoMgrPool("testmongopool", "127.0.0.1:27017", "test", "students", "usr", "passwd", MONGO_NUM)
 
--- test/mysqltest.go
+++ test/mysqltest.go
@@ -10,9 +10,9 @@ import (
 )
 
 const (
-	COR_NUM    = 100
-	MYSQL_NUM  = 10
-	ACTION_NUM = 10000
+	COR_NUM    = 10
+	MYSQL_NUM  = 3
+	ACTION_NUM = 1000000
 )
 
 type Student struct {
@@ -27,13 +27,16 @@ func MysqlInsert(idx int, pool *zed.MysqlMgrPool, wg *sync.WaitGroup) {
 	idx = idx * ACTION_NUM * 10
 
 	for i := 0; i < ACTION_NUM/COR_NUM; i++ {
+		time.Sleep(time.Second * 3)
 		pool.DBAction(n, func(db *mysql.Conn) {
 			//rows, res, err := db.Query("insert xx values(\"usr%d\", %d);", i, i%30+20)
 			_, _, err := (*db).Query("insert xx values(\"usr%d\", %d);", n*ACTION_NUM+(i+1), i%30+20)
-
+			fmt.Println("insert err:", n, err)
 			if err != nil {
-				fmt.Println("insert err:", n, err)
+
+				panic(err)
 			}
+
 		})
 
 		/*_, _, err := (*pool.GetMgr(0).DB).Query("insert xx values(\"usr%d\", %d);", n*ACTION_NUM+(i+1), i%30+20)
@@ -45,6 +48,31 @@ func MysqlInsert(idx int, pool *zed.MysqlMgrPool, wg *sync.WaitGroup) {
 }
 
 func main() {
+	zed.Init("./", "./log")
+	//zed.MakeNewLogDir("./")
+	const (
+		TagNull = iota
+		Tag1
+		Tag2
+		Tag3
+		TagMax
+	)
+	var LogTags = map[int]string{
+		TagNull: "zed",
+		Tag1:    "Tag1",
+		Tag2:    "Tag2",
+		Tag3:    "Tag3",
+	}
+	var LogConf = map[string]int{
+		//"Info":   zed.LogCmd,
+		"Info":   zed.LogFile,
+		"Warn":   zed.LogFile,
+		"Error":  zed.LogFile,
+		"Action": zed.LogFile,
+	}
+
+	zed.StartLogger(LogConf, true, TagMax, LogTags, 3, 3, 3, 3)
+
 	//ch := make(chan int)
 	//pool := zed.NewMysqlMgrPool("testmongopool", "127.0.0.1:27017", "test", "students", "usr", "passwd", MONGO_NUM)
 	pool := zed.NewMysqlMgrPool("testmysqlpool", "127.0.0.1:3306", "test", "root", "", MYSQL_NUM)
