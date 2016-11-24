47e804d389a2f8c1ab2fe43bd379ca09259b6b6c
cokeboL <165128167@qq.com>
2016-11-24 12:37:43 +0800

    1

--- tcpclient.go
+++ tcpclient.go
@@ -97,7 +97,8 @@ func (client *TcpClient) writer() {
 
 func (client *TcpClient) SendMsg(msg *NetMsg) {
 	ZLog("[Send_1] %s Cmd: %d Len: %d", client.Info(), msg.Cmd, msg.Len)
-	client.parent.SendMsg(client, msg)
+	//client.parent.SendMsg(client, msg)
+	client.SendMsgAsync(msg)
 	/*
 			var (
 				writeLen = 0
