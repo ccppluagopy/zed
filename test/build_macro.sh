7a959fd70f450f8a3b45bc7d7276b7531ff0fb75
cokeboL <165128167@qq.com>
2017-06-03 14:43:24 +0800

    1

--- test/build_macro.sh
+++ test/build_macro.sh
@@ -7,3 +7,7 @@ mkdir bin
 cp config.json start.sh stop.sh bin/
 zip bin.zip bin -r
 rm -rf bin 
+
+#windows
+#set BUILD_TIME=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,8%
+#go build -ldflags "-X main.BuildTime=%BUILD_TIME%" -o ./main.bin main.go
