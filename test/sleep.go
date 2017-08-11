3c70f5dd974bd1b2347bfeb66f192626c57a37e7
cokeboL <165128167@qq.com>
2017-08-11 17:54:03 +0800

    fix time.Async deadlock

--- test/sleep.go
+++ test/sleep.go
@@ -3,14 +3,37 @@
 	需要删除go/pkg对应的time库文件，再go install time才会生效
 */
 
+var (
+	asyncCh chan func()  
+)
+
+func InitAsync(buf int) {
+	if asyncCh == nil {
+		asyncCh = make(chan func, 100)
+		go func() {
+			for {
+				if cb, ok := <- asyncCh; ok {
+					cb()
+				} else {
+					break
+				}
+			}
+		}
+	}
+}
+
+
 func Async(f func()) {
 	var t *Timer
 	t = &Timer{
 		r: runtimeTimer{
 			when: when(1),
 			f: func(arg interface{}, seq uintptr) {
-				arg.(func())()
-				t.Stop()
+				asyncCh <- func() {
+					defer func() { recover() }
+					arg.(func())()
+					t.Stop()
+				}
 			},
 			arg: f,
 		},
@@ -23,7 +46,10 @@ func AfterFuncWithoutGo(d Duration, f func()) *Timer {
 		r: runtimeTimer{
 			when: when(d),
 			f: func(arg interface{}, seq uintptr) {
-				arg.(func())()
+				asyncCh <- func() {
+					defer func() { recover() }
+					arg.(func())()
+				}
 			},
 			arg: f,
 		},
