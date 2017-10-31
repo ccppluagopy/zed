9cfff1c12921bb5f5cfb454cd7f55228605f101b
Joke.G <Joke.G@iv66.net>
2017-10-31 18:01:51 +0800

    + test

---
+++ test/test_newlogger.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	log "github.com/ccppluagopy/zed/logger"
+)
+
+func main() {
+	log.SetLogDir("./logs/", true)
+	log.SetLogType(log.LogCmd | log.LogFile)
+	log.Info(" %d %s", 111, "hahaha")
+}
