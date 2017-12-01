08225991ee2e85bca79cbabb52cc10e34c29b414
cokeboL <165128167@qq.com>
2017-12-01 22:05:22 +0800

    -

--- test/blog.md
+++ test/blog.md
@@ -1,3 +1,6 @@
+如何实现支持数亿用户的长连消息系统 | Golang高并发案例 360消息推送系统
+http://chuansong.me/n/1641640
+
 go kafka
 http://legendtkl.com/2016/06/30/kafka-in-action/
 
--- test/test_telegram.go
+++ test/test_telegram.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
+	"time"
 )
 
 //https://api.telegram.org/botgetMe/387654221:AAHpOV5QcGTzG3CQIUso9SgjOonVKxyLVSs
+
+var (
+	users = map[string]string{}
+)
 
 func main() {
 
@@ -25,18 +30,31 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
-	n := 0
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		n++
-		fmt.Printf("chat: %+v", update.Message.Chat)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("hello %d", n))
-		msg.ReplyToMessageID = update.Message.MessageID
+		//log.Printf("----[%s]: %s", update.Message.From.UserName, update.Message.Text)
+		fmt.Println("\n**************************************")
+		fmt.Printf("------ from: %+v\n", update.Message.From)
+		fmt.Printf("------ contact: %+v\n", update.Message.Contact)
+		fmt.Printf("------ chatid: %+v\n", update.Message.Chat.ID)
+		fmt.Printf("------ text: %+v\n", update.Message.Text)
+		fmt.Println("\n**************************************")
 
-		bot.Send(msg)
+		go func() {
+			n := 0
+			for {
+				n++
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("hello %d", n))
+				//msg.ReplyToMessageID = update.Message.MessageID
+
+				ret, err := bot.Send(msg)
+				fmt.Println("send ", n, err, ret)
+				time.Sleep(time.Second * 10)
+			}
+		}()
 	}
 }
